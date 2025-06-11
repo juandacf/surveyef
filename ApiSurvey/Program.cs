66134fd50700c942f7d44ea691ad35edb05ffe0f
Juan Caballero <juandacf@gmail.com>
2025-06-10 20:07:41 -0500

    rate limiter injected to main program

--- ApiSurvey/Program.cs
+++ ApiSurvey/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddRateLimiter();
+
 
 builder.Services.AddDbContext<TallerSurveyDbContext>(options =>
 {
